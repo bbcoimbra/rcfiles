911062249f55a26d31dbf156233c86406bed0325
Bruno Coimbra <bbcoimbra@gmail.com>
2016-06-08 18:41:15 -0300

    fix rvm settings

--- bash.d/ruby.bash
+++ bash.d/ruby.bash
@@ -1,12 +1,15 @@
 ### Ruby Settings
-# rvm Settings
-[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 # irb Settings
 export IRBRC="$HOME/.irbrc"
 
-_RVM_PROMPT="${RED}\$(~/.rvm/bin/rvm-prompt u g)"
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
 ### Heroku path
 PATH="/usr/local/heroku/bin:$PATH"
 export PATH
+
+# rvm Settings
+if [[ -s $HOME/.rvm/scripts/rvm ]]; then
+	source $HOME/.rvm/scripts/rvm
+	_RVM_PROMPT="${RED}\$(~/.rvm/bin/rvm-prompt u g)"
+fi
