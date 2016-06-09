6b048554bd13842f82e68261e4d4d85036ce801e
Bruno Coimbra <bbcoimbra@gmail.com>
2016-06-09 01:29:59 -0300

    rvm is not needed

--- bash_config/bash.d/ruby.bash
+++ bash_config/bash.d/ruby.bash
@@ -8,10 +8,3 @@ eval "$(rbenv init -)"
 ### Heroku path
 PATH="/usr/local/heroku/bin:$PATH"
 export PATH
-
-# rvm Settings
-if [[ -s $HOME/.rvm/scripts/rvm ]]; then
-	source $HOME/.rvm/scripts/rvm
-	_RVM_PROMPT="${RED}\$(~/.rvm/bin/rvm-prompt u g)"
-	PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
-fi
