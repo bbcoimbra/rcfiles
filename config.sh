f70fc23d33edea7b04068fa6790842ba9456236e
Bruno Coimbra <bbcoimbra@gmail.com>
2016-06-09 01:20:15 -0300

    main config script

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+CUR_DIR="$(pwd)"
+for file in gemrc gitconfig gitignore irbrc tmux.conf vimrc ; do
+	echo -ne "linking '$CUR_DIR/$file' -> '$HOME/.$file'\n"
+  ln -F -s "$CUR_DIR/$file" ~/.$file
+done
+
+for dir in $(find . \( -not -regex '.*/\..*' \) -type d -d 1) ; do
+	echo -ne "cd $dir && ./config.sh (in subshell)\n"
+  ( cd $dir && ./config.sh )
+done
