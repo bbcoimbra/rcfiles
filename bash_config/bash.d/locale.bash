d8adec7d9f555a57372b777aa222ded432bbba8c
Bruno Coimbra <bbcoimbra@gmail.com>
2016-06-16 19:15:02 -0300

    fix perl warnings (because LC_ALL in ack)

--- bash_config/bash.d/locale.bash
+++ bash_config/bash.d/locale.bash
@@ -2,6 +2,6 @@
 LANG='en_US.utf8'
 GDM_LANG='en_US.utf8'
 LC_COLLATE=C # for ascii sort
+LC_ALL=C # for ascii sort
 LANGUAGE='en_US.utf8'
-export LANG GDM_LANG LC_COLLATE LANGUAGE
-
+export LANG GDM_LANG LC_COLLATE LANGUAGE LC_ALL
