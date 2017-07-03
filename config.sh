b279853cdfb04b3e606124f1fef629dd14aee186
Bruno Coimbra <bbcoimbra@gmail.com>
2017-07-03 18:01:30 -0300

    force creation of symbolic links

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ set -e
 CUR_DIR="$(pwd)"
 for file in gemrc gitconfig gitignore irbrc tmux.conf muttrc ackrc ; do
 	echo -ne "linking '$CUR_DIR/$file' -> '$HOME/.$file'\n"
-  ln -F -s "$CUR_DIR/$file" ~/.$file
+  ln -f -F -s "$CUR_DIR/$file" ~/.$file
 done
 
 for dir in $(find . \( -not -regex '.*/\..*' \) -type d -depth 1) ; do
